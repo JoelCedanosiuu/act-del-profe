c57ebdfa0259ce87167caab817905e38c46d6cb8
Joelcedano <joelcedanomendiola@gmail.com>
2025-10-02 07:59:17 -0700

    act4.py

---
+++ act4.py
@@ -0,0 +1,13 @@
+maestro = {
+    'nombre': 'Joel',
+    'matricula': '241690336-3',
+    'edad': '16',
+    'sexo': 'Masculino',
+    'telefono': '311 315 26 70',
+    'domicilio': 'provincia de barcelona #67',
+    'carrera': 'ciencia de datos e inteligencia artificial',
+    'semestre': '8',
+    'fecha de cumpleaños': '5 de marzo'
+}
+
+print(maestro['fecha de cumpleaños'])
