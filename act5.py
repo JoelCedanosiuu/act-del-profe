4cb44643865dfb51ffa6c26535354eb5d837d028
Joelcedano <joelcedanomendiola@gmail.com>
2025-10-03 13:07:56 -0700

    Create act5.py

---
+++ act5.py
@@ -0,0 +1,17 @@
+temp_fahrenheit = [80, 72, 95, 82, 79, 72, 85]
+
+
+temp_centigrados = []
+
+
+for fahrenheit in temp_fahrenheit:
+
+
+ centigrados = (fahrenheit - 32) * 5/9
+
+ #use el round para redondear los numeros
+ temp_centigrados.append(round(centigrados, 2))
+
+
+print(temp_fahrenheit)
+print(temp_centigrados)
