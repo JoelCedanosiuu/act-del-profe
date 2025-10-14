64a699761a4789029af8e11d3a10a7f2ccf7bda7
Joelcedano <joelcedanomendiola@gmail.com>
2025-10-14 07:49:01 -0700

    Create act6.py

---
+++ act6.py
@@ -0,0 +1,51 @@
+Tarea-1: Crear una lista vacía
+
+shopping_list = []
+
+
+Tarea-2: Agregar los elementos
+
+shopping_list = ["Reloj", "Portátil", "Calzado", "Pluma", "Ropa"]
+
+
+Tarea-3: Agregar “Fútbol”
+
+shopping_list.append("Fútbol")
+
+
+Tarea-4: Imprimir el primer elemento
+
+print(shopping_list[0])
+
+
+Tarea-5: Imprimir el último elemento
+
+print(shopping_list[-1])
+
+
+Tarea-6: Imprimir toda la lista
+
+print(shopping_list)
+
+
+Tarea-7: Imprimir los artículos importantes (“Portátil” y “Calzado”)
+
+print(shopping_list[1])  # Portátil
+print(shopping_list[2])  # Calzado
+
+
+Tarea-8: Cambiar “Pluma” por “Cuaderno”
+
+shopping_list[3] = "Cuaderno"
+print(shopping_list)
+
+
+Tarea-9: Eliminar “Ropa”
+
+shopping_list.remove("Ropa")
+print(shopping_list)
+
+
+Tarea-10: Imprimir la lista final
+
+print("Lista final de compras:", shopping_list)
